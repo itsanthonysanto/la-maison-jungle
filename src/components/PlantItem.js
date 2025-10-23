9a478cd402c16aba5a135140823f60206fc636c7
itsanthonysanto <anthony.santo@hotmail.fr>
2025-10-23 13:35:20 +0200

    P3C2

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -5,11 +5,12 @@ function handleClick(plantName) {
 	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ cover, name, water, light }) {
+function PlantItem({ cover, name, water, light, price, category }) {
 	return (
 		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
-			{name}
+			{name} {category}
 			<div>
 				<CareScale careType='water' scaleValue={water} />
 				<CareScale careType='light' scaleValue={light} />
